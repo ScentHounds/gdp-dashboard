e11955fbcdb47c356e7dab7dba058e76989edde6
ScentHounds <84450243+ScentHounds@users.noreply.github.com>
2025-11-13 13:51:43 +0100

    Change GDP dashboard to ASP dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='ASP dashboard',
+    #page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
@@ -14,7 +14,7 @@ st.set_page_config(
 
 @st.cache_data
 def get_gdp_data():
-    """Grab GDP data from a CSV file.
+    """Grab ASP data from a XLSX file.
 
     This uses caching to avoid having to read the file every time. If we were
     reading from an HTTP endpoint instead of a file, it's a good idea to set
@@ -22,11 +22,11 @@ def get_gdp_data():
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'C:\Users\Bloodhound\Documents\Tájegységi dolgok\ASP_data'
+    raw_ASP_df = pd.read_excel(DATA_FILENAME)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    MIN_YEAR = 2017
+    MAX_YEAR = 2025
 
     # The data above has columns like:
     # - Country Name
@@ -45,26 +45,26 @@ def get_gdp_data():
     # - GDP
     #
     # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
+    ASP_df = raw_ASP_df.melt(
+        ['Település'],
         [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
+        'Megerősítés dátuma',
+        'VT kód',
     )
 
     # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+    ASP_df['Megerősítés dátuma'] = pd.to_numeric(ASP_df['Megerősítés dátuma'])
 
-    return gdp_df
+    return ASP_df
 
-gdp_df = get_gdp_data()
+ASP_df = get_ASP_data()
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
 
 # Set the title that appears at the top of the page.
 '''
-# :earth_americas: GDP dashboard
+# :earth_americas: ASP dashboard
 
 Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
 notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
@@ -75,8 +75,8 @@ But it's otherwise a great (and did I mention _free_?) source of data.
 ''
 ''
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+min_value = ASP_df['Megerősítés dátuma'].min()
+max_value = ASP_df['Megerősítés dátuma'].max()
 
 from_year, to_year = st.slider(
     'Which years are you interested in?',
@@ -84,67 +84,67 @@ from_year, to_year = st.slider(
     max_value=max_value,
     value=[min_value, max_value])
 
-countries = gdp_df['Country Code'].unique()
+City = ASP_df['Település'].unique()
 
-if not len(countries):
+if not len(City):
     st.warning("Select at least one country")
 
-selected_countries = st.multiselect(
+selected_City = st.multiselect(
     'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+    Cities,
+    ['Gyermely', 'Budakeszi', 'Szentendre', 'Zsámbék', 'Süttő', 'Tarján'])
 
 ''
 ''
 ''
 
 # Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
+filtered_ASP_df = ASP_df[
+    (ASP_df['Település'].isin(selected_City))
+    & (ASP_df['Megerősítés dátuma'] <= to_year)
+    & (from_year <= ASP_df['Megerősítés dátuma'])
 ]
 
-st.header('GDP over time', divider='gray')
+st.header('ASP over time', divider='gray')
 
 ''
 
 st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
+    filtered_ASP_df,
+    x='Megerősítés dátuma',
+    y='VT kód',
+    color='Település',
 )
 
 ''
 ''
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+first_year = ASP_df[ASP_df['Megerősítés dátuma'] == from_year]
+last_year = ASP_df[ASP_df['Megerősítés dátuma'] == to_year]
 
-st.header(f'GDP in {to_year}', divider='gray')
+st.header(f'ASP in {to_year}', divider='gray')
 
 ''
 
 cols = st.columns(4)
 
-for i, country in enumerate(selected_countries):
+for i, City in enumerate(selected_City):
     col = cols[i % len(cols)]
 
     with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+        first_ASP = first_year[first_year['Település'] == country]['ASP'].iat[0] / 1000000000
+        last_ASP = last_year[last_year['Település'] == country]['ASP'].iat[0] / 1000000000
 
-        if math.isnan(first_gdp):
+        if math.isnan(first_ASP):
             growth = 'n/a'
             delta_color = 'off'
         else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
+            growth = f'{last_ASP / first_ASP:,.2f}x'
             delta_color = 'normal'
 
         st.metric(
-            label=f'{country} GDP',
+            label=f'{country} ASP',
             value=f'{last_gdp:,.0f}B',
             delta=growth,
             delta_color=delta_color
